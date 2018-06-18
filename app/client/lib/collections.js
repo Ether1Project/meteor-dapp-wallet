9833cb472b117d121fdf8cef0d547a1b8fb902a8
suijiaza <40181225+suijiaza@users.noreply.github.com>
2018-06-18 06:36:34 -0700

    Update collections.js

--- app/client/lib/collections.js
+++ app/client/lib/collections.js
@@ -4,25 +4,25 @@
 
 
 Wallets = new Mongo.Collection('wallets', {connection: null});
-new PersistentMinimongo2(Wallets, 'pirl_wallet');
+new PersistentMinimongo2(Wallets, 'etho_wallet');
 
 CustomContracts = new Mongo.Collection('custom-contracts', {connection: null});
-new PersistentMinimongo2(CustomContracts, 'pirl_wallet');
+new PersistentMinimongo2(CustomContracts, 'etho_wallet');
 
 // Contains the transactions
 Transactions = new Mongo.Collection('transactions', {connection: null});
-new PersistentMinimongo2(Transactions, 'pirl_wallet');
+new PersistentMinimongo2(Transactions, 'etho_wallet');
 
 // Contains the pending confirmations
 PendingConfirmations = new Mongo.Collection('pending-confirmations', {connection: null});
-new PersistentMinimongo2(PendingConfirmations, 'pirl_wallet');
+new PersistentMinimongo2(PendingConfirmations, 'etho_wallet');
 
 // Contains the custom contract events
 Events = new Mongo.Collection('events', {connection: null});
-new PersistentMinimongo2(Events, 'pirl_wallet');
+new PersistentMinimongo2(Events, 'etho_wallet');
 
 // Contains Coin Information
 Tokens = new Mongo.Collection('tokens', {connection: null});
-new PersistentMinimongo2(Tokens, 'pirl_wallet');
+new PersistentMinimongo2(Tokens, 'etho_wallet');
 
 
